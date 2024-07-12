60d51afd7090181b782f80e90ed03cae74310912
Prajwal Tiwari <fnafchica2004@gmail.com>
2024-07-12 23:17:27 +0530

    added components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import ChatBot from './components/ChatBot';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ChatBot />
+    </div>
+  );
+}
+
+export default App;
